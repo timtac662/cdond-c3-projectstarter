4a4e7139a98b6ffed1cc103972253bb56cb5656c
Akintolu Timilehin <timtac662@gmail.com>
2022-07-22 23:00:09 +0100

    smoke

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,9 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_HOST=udacity-akintolu.cskp3ss0gp7z.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=telecom66
+export TYPEORM_DATABASE=my_udapeople_database
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_MIGRATIONS_DIR=./src/migrations 
